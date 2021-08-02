b32f595159180ed5e7e2068808ad2f9985099c87
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-02 11:26:39 +0300

    Added a method to save users

--- credential.py
+++ credential.py
@@ -1,3 +1,15 @@
 class Credentials:
     
     credential_details = [] #initialize an array to store the credential details
+     def __init__(self,credentials_account, credentials_username, credentials_password):
+        '''
+        This will initialize the credentials details required for the credentials class
+        '''
+        self.credentials_account = credentials_account
+        self.credentials_username = credentials_username
+        self.credentials_password = credentials_password
+     def save_credentials(self):
+         '''
+         Save the credentials of a user
+         '''
+         Credentials.credential_details.append(self)
