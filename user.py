ea58d51a8890862cf5d2eaa2ce7d9a5acfcca400
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-02 08:44:46 +0300

    created an inatance in the user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    user_list = []# Array to store the users accounts
+    def __init__(self, firstName, lastName, username):
+        '''
+        This is used to initialize the properties of the class credentials
+        firstName --> initialize the first name of the user
+        lastName --> initialize the last name of the user
+        user --> initialize the user name of the user
+        '''
+        self.firstName = firstName
+        self.lastName = lastName
+        self.username = username
